e58f298242dda78185552f6c5594aee8ee8a9146
Gabe Kubiak <kubiakga@msu.edu>
2024-01-19 17:54:06 -0500

    GO INTO /troisjs in terminal. npm install and npm run dev. that localhost will have the model

---
+++ troisjs/src/App.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <Renderer ref="renderer" antialias :orbit-ctrl="{ enableDamping: true }" resize>
+    <Camera :position="{ x: 20, y: 0, z: 0 }" />
+    <Scene>
+      <AmbientLight></AmbientLight>
+      <GltfModel src="../src/Models/Excavator.glb" />
+    </Scene>
+  </Renderer>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { Box, Camera, LambertMaterial, PointLight, Renderer, Scene } from 'troisjs'
+const rendererC = ref()
+const meshC = ref()
+onMounted(() => {
+  const renderer = rendererC.value
+  const mesh = meshC.value.mesh
+  renderer.onBeforeRender(() => {
+    mesh.rotation.x += 0.01
+  })
+})
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+canvas {
+  display: block;
+}
+</style>
+
+<script>
+import {
+  AmbientLight,
+  Camera,
+  GltfModel,
+  Renderer,
+  Scene,
+} from 'troisjs';
+
+export default {
+  components: {
+    AmbientLight,
+    Camera,
+    GltfModel,
+    Renderer,
+    Scene,
+  },
+};
+</script>
---
+++ troisjs/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'troisjs'
+import App from './App.vue'
+// import { GLTFLoader } from '../js/examples/jsm/loaders/GLTFLoader.js';
+
+createApp(App).mount('#app')
+// var loader = new GLTFLoader();
+            
+// loader.load( '../Models/Excavator.glb', function ( gltf )
+// {
+//     sword = gltf.scene;  // sword 3D object is loaded
+//     sword.scale.set(2, 2, 2);
+//     sword.position.y = 4;
+//     scene.add(sword);
+// } );
+
