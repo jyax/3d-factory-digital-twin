6b6434eb50170129e36ba76637552b7d5e43f324
Cody <captaincodygirard@gmail.com>
2024-02-01 20:52:05 -0500

    Display floor in orillusion. Cameras need to be tweaked to make viewing smoother

---
+++ orillusion/src/App.vue
@@ -0,0 +1,16 @@
+
+
+<script lang="ts" setup>
+  import { onMounted } from 'vue';
+  import ImportFloor from './load_floor';
+
+  onMounted(()=>{
+    new ImportFloor().run();
+  })
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
