3467831e0686f090827c0ee241334011a69a172d
Cody Girard <girardc4@msu.edu>
2024-02-04 22:15:07 -0500

    Change in lighting and updated code. Attempted to implement moving models.

--- orillusion/src/App.vue
+++ orillusion/src/App.vue
@@ -3,9 +3,13 @@
 <script lang="ts" setup>
   import { onMounted } from 'vue';
   import ImportFloor from './load_floor';
+  import Sample_MousePick from './test';
+  import Sample_AddRemove from './test2';
 
   onMounted(()=>{
     new ImportFloor().run();
+    // new Sample_MousePick().run();
+    // new Sample_AddRemove().run();
   })
 </script>
 
--- orillusion/src/load_floor.ts
+++ orillusion/src/load_floor.ts
@@ -1,13 +1,21 @@
-import { Engine3D, Scene3D, Object3D, Camera3D, DirectLight, PointLight, HoverCameraController, View3D, Vector3, Color }
+import { AtmosphericComponent, Engine3D, Scene3D, Object3D, Camera3D, DirectLight, PointLight, HoverCameraController, View3D, Vector3, Color, ColliderComponent, PointerEvent3D, MeshRenderer, UnLitTexArrayMaterial, }
     from "@orillusion/core";
  
 export default class ImportFloor {
     async run() {
+        // enable pick and use pixel mode
+        Engine3D.setting.pick.enable = true;
+        Engine3D.setting.pick.mode = `pixel`;
+
         console.log('hello 3d');
         // Initialize engine
         await Engine3D.init();
         // Create scene
         let scene: Scene3D = new Scene3D();
+
+        // add atmospheric sky
+        let sky = scene.addComponent(AtmosphericComponent);
+        sky.sunY = 50;
 
         // Create camera
         let cameraObj: Object3D = new Object3D();
@@ -24,6 +32,11 @@ export default class ImportFloor {
         // Load glb model
         let floor = await Engine3D.res.loadGltf('./../glb_models/factory_floor_sample_1.glb');
         scene.addChild(floor);
+
+        // Load glb model
+        let table = await Engine3D.res.loadGltf('./../glb_models/assembly_warehouse_table.glb');
+        table.addComponent(ColliderComponent);
+        scene.addChild(table);
 
         // add direct light component
         let dlight: Object3D = new Object3D();
@@ -46,7 +59,65 @@ export default class ImportFloor {
         let view = new View3D();
         view.scene = scene;
         view.camera = camera;
+        
         // Render
         Engine3D.startRenderView(view);
+
+        view.pickFire.addEventListener(PointerEvent3D.PICK_OVER, this.onOver, this);
+        view.pickFire.addEventListener(PointerEvent3D.PICK_UP, this.onUp, this);
+        view.pickFire.addEventListener(PointerEvent3D.PICK_DOWN, this.onDown, this);
+        view.pickFire.addEventListener(PointerEvent3D.PICK_CLICK, this.onPick, this);
+        view.pickFire.addEventListener(PointerEvent3D.PICK_OUT, this.onOut, this);
+        view.pickFire.addEventListener(PointerEvent3D.PICK_MOVE, this.onMove, this);
+
+    }
+
+    private onOver(e: PointerEvent3D) {
+        console.log('onOver', e.target.name, e.data.pickInfo);
+        // let obj = e.target as Object3D;
+        // let mr = obj.getComponent(MeshRenderer);
+        // let mat = new UnLitTexArrayMaterial();
+        // mat.baseColor = Color.COLOR_RED;
+        // mr.material = mat;
+    }
+
+    private onUp(e: PointerEvent3D) {
+        console.log('onUp', e.target.name, e.data.pickInfo);
+        // let obj = e.target as Object3D;
+        // let mr = obj.getComponent(MeshRenderer);
+        // let mat = new UnLitTexArrayMaterial();
+        // mat.baseColor = Color.COLOR_RED;
+        // mr.material = mat;
+    }
+
+    private onDown(e: PointerEvent3D) {
+        console.log('onDown', e.target.name, e.data.pickInfo);
+        // let obj = e.target as Object3D;
+        // let mr = obj.getComponent(MeshRenderer);
+        // let mat = new UnLitTexArrayMaterial();
+        // mat.baseColor = Color.COLOR_RED;
+        // mr.material = mat;
+    }
+
+    private onPick(e: PointerEvent3D) {
+        console.log('onPick', e.target.name, e.data.pickInfo);
+        // let obj = e.target as Object3D;
+        // let mr = obj.getComponent(MeshRenderer);
+        // let mat = new UnLitTexArrayMaterial();
+        // mat.baseColor = Color.COLOR_RED;
+        // mr.material = mat;
+    }
+
+    private onOut(e: PointerEvent3D) {
+        console.log('onOut', e.target.name, e.data.pickInfo);
+        // let obj = e.target as Object3D;
+        // let mr = obj.getComponent(MeshRenderer);
+        // let mat = new UnLitTexArrayMaterial();
+        // mat.baseColor = Color.COLOR_RED;
+        // mr.material = mat;
+    }
+
+    private onMove(e: PointerEvent3D) {
+        console.log('onMove', e.target.name, e.data.pickInfo);
     }
 }
