c4837609c6b6845e5f9b86ebab226574f1ed9474
kubiakga <kubiakga@msu.edu>
2024-02-15 14:55:26 -0500

    Subscribers working

--- mqtt-emulator/Publisher.py
+++ mqtt-emulator/Publisher.py
@@ -5,23 +5,23 @@ import TEST_GUI
 from paho.mqtt import client as mqtt_client
 
 
-broker = 'localhost'
+broker = '35.9.22.105'
 port = 1883
 topic = "python/mqtt"
 # Generate a Client ID with the publish prefix.
 client_id = f'publish-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
+username = 'root'
+password = 'M0nG0$$w0rd'
 
 def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(client, userdata, flags, rc,properties=None):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
+    client = mqtt_client.Client(client_id,protocol=mqtt_client.MQTTv5)
+    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
