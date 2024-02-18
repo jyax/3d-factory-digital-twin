feb51828efb89f7975fc0c992d2707a964464773
kubiakga <kubiakga@msu.edu>
2024-02-18 00:20:08 -0500

    MQTT hates localhost pt2 electric boogaloo - working toggle test mode in handler and python files to use on local docker container instead of server

--- mqtt-emulator/Publisher.py
+++ mqtt-emulator/Publisher.py
@@ -4,15 +4,25 @@ import time
 import TEST_GUI 
 from paho.mqtt import client as mqtt_client
 
+###TOGGLE TEST MODE###
+######################
+serverless  =  True###
+######################
 
-broker = '35.9.22.105'
+if not serverless:
+    broker = '35.9.22.105'
+else:
+    broker = 'localhost'
 port = 1883
 topic = "python/mqtt"
+
+
 # Generate a Client ID with the publish prefix.
 client_id = f'publish-{random.randint(0, 1000)}'
 username = 'root'
 password = 'M0nG0$$w0rd'
 
+## Connect to client
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc,properties=None):
         if rc == 0:
@@ -26,22 +36,24 @@ def connect_mqtt():
     client.connect(broker, port)
     return client
 
-
+## publish to client
 def publish(client, msg):
     result = client.publish(topic, msg)
     # result: [0, 1]
     status = result[0]
     if status == 0:
-        print(f"Send `{msg}` to topic `{topic}`")
+        print(f"Sent `{msg}` to topic `{topic}`")
     else:
         print(f"Failed to send message to topic {topic}")
         
-
+## Code to run on launch
 def run():
     client = connect_mqtt()
     client.loop_start()
     TEST_GUI.main(client)
     client.loop_stop()
+
+
 
 if __name__ == '__main__':
     run()
