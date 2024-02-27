901a1232187a83a5b718145f4e9b9ffdfcd98b35
Alan Feng <fengalan@msu.edu>
2024-02-26 20:58:11 -0500

    spent the entire day not getting technical contributions :(

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+    },
+    plugins: [vue()],
+  }
 })
