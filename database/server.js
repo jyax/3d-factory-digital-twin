0b6c0f50d00c134583c58e03eb91ccb869f5783a
Alan Feng <fengalan@msu.edu>
2024-03-05 18:59:09 -0500

    added code to send and receive data

--- database/server.js
+++ database/server.js
@@ -1,25 +1,32 @@
 // server.js
 
-import express from 'express';
-import { MongoClient } from 'mongodb';
-import ModelLoader from './ModelLoader';
+//import socket from 'socket.io';
+const socket = require('socket.io');
+const express = require('express');
+//import { MongoClient } from 'mongodb';
+//import ModelLoader from './ModelLoader';
+const cors = require('cors');
+const { AddToDirectory } = require('database/AddToDirectory.js');
 
 const app = express();
-const PORT = 5173;
+app.use(cors());
+app.use(express.json());
+const PORT = 3000;
 const databaseUrl = 'mongodb://root:password@mongodb:27017'; // Use the service name defined in Docker Compose
 
 // Initialize ModelLoader with database URL
-const modelLoader = new ModelLoader(databaseUrl);
+//const modelLoader = new ModelLoader(databaseUrl);
 
 // API endpoint to load models from MongoDB
-app.get('/api/loadModels', async (req, res) => {
-    try {
-        await modelLoader.loadModelsFromMongoDB();
-        res.json({ message: 'Models loaded successfully.' });
-    } catch (error) {
-        console.error('Error loading models:', error);
-        res.status(500).json({ error: 'Failed to load models.' });
-    }
+app.post('/api/loadModels', async (req, res) => {
+    console.log(req.body);
+    // try {
+    //     await modelLoader.loadModelsFromMongoDB();
+    //     res.json({ message: 'Models loaded successfully.' });
+    // } catch (error) {
+    //     console.error('Error loading models:', error);
+    //     res.status(500).json({ error: 'Failed to load models.' });
+    // }
 });
 
 app.listen(PORT, () => {
