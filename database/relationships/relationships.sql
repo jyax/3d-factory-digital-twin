b12cc73f8082b4ef906966b0f8f0023c1467a481
kubiakga <kubiakga@msu.edu>
2024-03-12 12:59:55 -0400

    preliminary remove stuff from versioning

--- database/relationships/relationships.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `relationships`(
-`model_id`  int(11)         NOT NULL AUTO_INCREMENT     COMMENT 'Model ID for identification',
-`model`     varchar(100)    NOT NULL                    COMMENT 'Model File, .obj/.stl/.glb/.gltf',
-`metadata`  varchar(100)    DEFAULT NULL                COMMENT 'Metadata JSON file',
-PRIMARY KEY (`model_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='File Relationships';
