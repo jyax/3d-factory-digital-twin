b873ca2a0a03e299d6ccd6a79469a0748cd5e667
Gabe Kubiak <kubiakga@msu.edu>
2024-03-31 20:51:35 -0400

    can run group of sequence objects

--- mqtt-emulator/changes.py
+++ mqtt-emulator/changes.py
@@ -4,6 +4,7 @@ class Sequence:
 	def __init__(self,states):
 		self.states = []
 		self.durations = []
+		self.start_times = [0]
 		
 	def add_state(self,state):
 		self.states.append(state)
@@ -40,27 +41,67 @@ class Sequence:
 		def getTime():
 			return time.perf_counter() - START_TIME
 
-		start_times = [0]
+		
 		total = 0
 		for t in self.durations:
 			total +=t
-			start_times.append(total)
+			self.start_times.append(total)
 
 		# self.states[0].liveUpdate(client)
 	   
 		while getTime() < LIMIT:
 			for idx in range(0,len(self.states)-1):
 				if(idx < len(self.durations)-1):
-					while start_times[idx] <= getTime() and getTime() < start_times[idx + 1]:
+					while self.start_times[idx] <= getTime() and getTime() < self.start_times[idx + 1]:
 						print('Animate OBJ',idx,'&',idx+1,'| Time:',getTime())
-						self.states[idx].animateSelf(self.states[idx+1],start_times[idx],self.durations[idx],getTime())
+						self.states[idx].animateSelf(self.states[idx+1],self.start_times[idx],self.durations[idx],getTime())
 						self.states[idx].liveUpdate(client)
 						time.sleep(CYCLE_TIME)
 				else:
-					while start_times[idx] <= getTime() and getTime() < LIMIT:
+					while self.start_times[idx] <= getTime() and getTime() < LIMIT:
 						print('Animate OBJ',idx,'&',idx+1,'| Time:',getTime())
-						self.states[idx].animateSelf(self.states[len(self.states)-1],start_times[idx],self.durations[idx],getTime())
+						self.states[idx].animateSelf(self.states[len(self.states)-1],self.start_times[idx],self.durations[idx],getTime())
 						self.states[idx].liveUpdate(client)
 						time.sleep(CYCLE_TIME)
 					
 		self.states[len(self.states)-1].liveUpdate(client)
+
+	
+def runAsGroup(sequences,client):
+
+	#constants
+		CYCLE_TIME = .05 # in seconds
+		START_TIME = time.perf_counter()
+		LIMIT = 30 # seconds
+
+		# GETTER for current time
+		def getTime():
+			return time.perf_counter() - START_TIME
+
+
+		for seq in sequences:
+			start_times = [0]
+			total = 0
+			for t in seq.durations:
+				total +=t
+				seq.start_times.append(total)
+
+
+	   
+		while getTime() < LIMIT:
+			for seq in sequences:
+				for idx in range(0,len(seq.states)-1):
+					if(idx < len(seq.durations)-1):
+						if seq.start_times[idx] <= getTime() and getTime() < seq.start_times[idx + 1]:
+		
+							seq.states[idx].animateSelf(seq.states[idx+1],seq.start_times[idx],seq.durations[idx],getTime())
+							seq.states[idx].liveUpdate(client)
+							
+					else:
+						if seq.start_times[idx] <= getTime() and getTime() < LIMIT:
+							seq.states[idx].animateSelf(seq.states[len(seq.states)-1],seq.start_times[idx],seq.durations[idx],getTime())
+							seq.states[idx].liveUpdate(client)
+
+				time.sleep(CYCLE_TIME)		
+					
+		
--- mqtt-emulator/test.py
+++ mqtt-emulator/test.py
@@ -7,17 +7,29 @@ import asset as a
 
 def testMain(client):
     s = Sequence([])
+    s2 = Sequence([])
 
     # for i in range(4):
     #     s.add_state('2')
 
-    s.add_state(a.Asset(rot_x=0))
-    s.add_state(a.Asset(rot_x=180))
-    s.add_state(a.Asset(rot_x=0))
-    s.add_state(a.Asset(rot_x=-180))
+    s.add_state(a.Asset(x=2450,y=170,z=-3500,rot_y=0))
+    s.add_state(a.Asset(x=2450,y=170,z=-3500,rot_y=180))
+    s.add_state(a.Asset(x=2450,y=170,z=-3500,rot_y=0))
+    s.add_state(a.Asset(x=2450,y=170,z=-3500,rot_y=180))
 
     s.add_duration(5)
     s.add_duration(5)
     s.add_duration(5)
 
-    s.runSolo(client)
+    s2.add_state(a.Asset(id=1,x=2450,y=170,z=-3500,rot_y=0))
+    s2.add_state(a.Asset(id=1,x=2450,y=170,z=-3500,rot_y=180))
+    s2.add_state(a.Asset(id=1,x=2450,y=170,z=-3500,rot_y=0))
+    s2.add_state(a.Asset(id=1,x=2450,y=170,z=-3500,rot_y=180))
+
+    s2.add_duration(5)
+    s2.add_duration(5)
+    s2.add_duration(5)
+
+    seqs = [s,s2]
+
+    runAsGroup(seqs,client)
