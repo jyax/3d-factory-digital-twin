fa33a1812709d46fca2d92fe77134831676a59ba
Logan Gillis <gillislo@msu.edu>
2024-04-03 20:44:54 -0400

    Added auth for DB.

--- dbconnect/index.js
+++ dbconnect/index.js
@@ -25,12 +25,7 @@ app.listen(port, ()=> console.log(host));
 
 // MongoDB Connection
 const mongodbURL = 'mongodb://34.162.94.101:27017';
-const client = new MongoClient(mongodbURL, {
-    auth: {
-        username: "admin",
-        password: "m@gn@_2024!"
-    }
-});
+const client = new MongoClient(mongodbURL);
 
 // Create bucket
 //const bucket = new mongodb.GridFSBucket(db);
